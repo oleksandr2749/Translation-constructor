ed41678f1eca602b950a43ae5c3d88849ae51e89
oleksandr2749 <oleksandr2749@gmail.com>
2024-05-30 02:28:33 +0300

    Версія 0.2.2 Покращення коду

--- Main.py
+++ Main.py
@@ -8,53 +8,35 @@ def get_data_from_file(path: str):
             content = file.readlines()
             return content
     except FileNotFoundError:
-        print("Файл не знайдено")
+        print("Помилка функції get_data_from_file")
         return None
 
-
-def get_comment(line: str):
-    comment = "<!--" + line + "-->"
-    return comment
-
-
-def get_def_name(def_line: str):
-    stage1 = def_line.replace("<defName>", "")
-    stage2 = stage1.replace("</defName>", "")
-    return stage2
-
-
-def get_label(def_name: str, label_line: str):
-    stage1 = label_line.replace("<label>", "")
-    stage2 = stage1.replace("</label>", "")
-    comment = get_comment(line=stage2)
-    label = "\n  " + comment + "\n  <" + def_name + ".label>" + stage2 + "</" + def_name + ".label>"
-    return label
-
-
-def get_description(def_name: str, description_line: str):
-    stage1 = description_line.replace("<description>", "")
-    stage2 = stage1.replace("</description>", "")
-    comment = get_comment(line=stage2)
-    description = "\n  " + comment + "\n  <" + def_name + ".description>" + stage2 + "</" + def_name + ".description>"
-    return description
-
-
-def data_filter(obtained_data):
-    data_filtering_stage1 = [item.strip() for item in obtained_data]
-    data_filtering_stage2 = []
-    for i in data_filtering_stage1:
+# Функція створення списку відфільтрованих даних отриманих від get_data_from_file
+def get_filtered_data(obtained_data):
+    data_strip = list()
+    data = list()
+    words_to_remove = ["<defName>", "</defName>", "<label>", "</label>", "<description>", "</description>"]
+    # Цикл видалення пробілів, табуляцій, переходів
+    for i in obtained_data:
+        data_strip.append(i.strip())
+    #
+    for i in data_strip:
         if "<defName>" in i or "<label>" in i or "<description>" in i:
             if "<label>point</label>" not in i and "<label>edge</label>" not in i:
-                data_filtering_stage2.append(i)
-    data = []
-    def_name = None
-    for i in data_filtering_stage2:
-        if "<defName>" in i:
-            def_name = get_def_name(i)
-        if "<label>" in i:
-            data.append(get_label(def_name, i))
-        if "description" in i:
-            data.append(get_description(def_name, i))
+                if "<defName>" in i:
+                    def_name = i
+                    for word in words_to_remove:
+                        def_name = def_name.replace(word, "")
+                if "<label>" in i:
+                    label = i
+                    for word in words_to_remove:
+                        label = label.replace(word, "")
+                    data.append("\n  " + "<!--" + label + "-->" + "\n  <" + def_name + ".label>" + label + "</" + def_name + ".label>")
+                if "<description>" in i:
+                    description = i
+                    for word in words_to_remove:
+                        description = description.replace(word, "")
+                    data.append("\n  " + "<!--" + description + "-->" + "\n  <" + def_name + ".description>" + description + "</" + def_name + ".description>")
     return data
 
 
@@ -64,20 +46,18 @@ def write_to_file(path: str, data: list):
             file.write("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<LanguageData>")
             for i in data:
                 file.write(i)
-            file.write("\n</LanguageData>")
+            file.write("\n</LanguageData>\n")
     except FileNotFoundError:
-        print("Файл не знайдено")
+        print("Помилка функції write_to_file")
         return None
 
 
 def run():
     # input_path_to_mod_file = (input("Вкажіть шлях до файлу моду: "))
     input_path_to_mod_file = "Buildings_Temperature.xml"
-    obtained_data = get_data_from_file(path=input_path_to_mod_file)
-    filtered_data = data_filter(obtained_data=obtained_data)
     # input_path_to_translation_file = (input("Вкажіть шлях до файлу перекладу: "))
     input_path_to_translation_file = "Test.xml"
-    write_to_file(path=input_path_to_translation_file, data=filtered_data)
+    write_to_file(path=input_path_to_translation_file, data=get_filtered_data(get_data_from_file(path=input_path_to_mod_file)))
 
 
 run()

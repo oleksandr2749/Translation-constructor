185015ba3636f69e86b5bb7739f6f82c0bb82fdd
oleksandr2749 <oleksandr2749@gmail.com>
2024-06-16 23:43:05 +0300

    Версія 0.3 Пробую передати об'єкт в клас вікна програми

--- ModificationClass.py
+++ ModificationClass.py
@@ -2,15 +2,74 @@ from pathlib import Path
 
 # Клас модифікації
 class Mod:
-    def __init__(self, packageid=None, path=None, name=None, author=None, supported_versions=None):
-        self.__PackageId = packageid
+
+    def __init__(self, path=None):
         self.__Path = path
-        self.__Name = name
-        self.__Author = author
-        self.__SupportedVersions = supported_versions
+        self.__PackageId = path.name
+        self.__Name = str()
+        self.__Author = str()
+        self.__SupportedVersion = str()
+        try:
+            with open(path/'About'/'About.xml', 'r', encoding='utf-8') as file:
+                name_is_set = False
+                author_is_set = False
+                is_author_str = False
+                supported_version_is_set = False
+                is_supported_version_str = False
+                for i in file.readlines():
+                    i = i.strip()
+                    if (i.startswith('<name>') and i.endswith('</name>')) or (i.startswith('<displayName>') and i.endswith('</displayName>') and (name_is_set is False)):
+                        self.__Name = i.replace('<displayName>', '').replace('</displayName>', '').replace('<name>', '').replace('</name>', '')
+                        name_is_set = True
+                        continue
+
+                    if author_is_set is False:
+                        if i.startswith('<author>') and i.endswith('</author>'):
+                            self.__Author = i.replace('<author>', '').replace('</author>', '')
+                            author_is_set = True
+                            continue
+
+                        if i.startswith('<authors>'):
+                            is_author_str = True
+                            continue
+                        elif i.endswith('</authors>'):
+                            self.__Author = self.__Author.removesuffix(', ')
+                            author_is_set = True
+                            is_author_str = False
+                            continue
+                        elif is_author_str is True:
+                            self.__Author += i.replace('<li>', '').replace('</li>', '') + ', '
+                            continue
+
+                    if supported_version_is_set is False:
+                        if i.startswith('<supportedVersions>'):
+                            is_supported_version_str = True
+                            continue
+                        elif i.endswith('</supportedVersions>'):
+                            self.__SupportedVersion = self.__SupportedVersion.removesuffix(' ')
+                            supported_version_is_set = True
+                            is_supported_version_str = False
+                            continue
+                        elif is_supported_version_str is True:
+                            self.__SupportedVersion += i.replace('<li>', '').replace('</li>', '') + ' '
+                            continue
+        except FileNotFoundError:
+            print('Помилка функції')
+
+    def __get_name(self, i):
+        pass
+
+    def __get_author(self, i):
+        pass
+
+    def __get_supported_version(self, i):
+        pass
 
     def return_atribute(self):
-        return self.__PackageId, self.__Path, self.__Name, self.__Author, self.__SupportedVersions
+        return 'Шлях -', self.__Path, 'Ід -', self.__PackageId, 'Назва -', self.__Name, 'Автор  -', self.__Author, 'Версії -', self.__SupportedVersion
+
+    def return_name(self):
+        return self.__Name
 
 
 # Функція отримання істинного шляху до теки модів гри.
@@ -22,84 +81,12 @@ def find_mod_folder():
             return path.resolve()
 
 
-def get_mod_name(path):
-    try:
-        with open(path, 'r', encoding='utf-8') as file:
-            for i in file.readlines():
-                if '<displayName>' in i or '<name>' in i:
-                    i = i.strip().replace('<displayName>', '').replace('</displayName>', '').replace('<name>', '').replace('</name>', '')
-                    return i
-    except FileNotFoundError:
-        print('Помилка функції write_to_file')
-        return None
-
-
-def get_mod_author(path):
-    is_name = False
-    a = str()
-    try:
-        with open(path, 'r', encoding='utf-8') as file:
-            for i in file.readlines():
-                i = i.strip()
-                if i.startswith('<author>') and i.endswith('</author>'):
-                    i = i.replace('<author>', '').replace('</author>', '')
-                    return i
-                if i.startswith('</authors>'):
-                    return a.removesuffix(', ')
-                if i.startswith('<authors>'):
-                    is_name = True
-                    continue
-                elif is_name is True:
-                    i = i.replace('<li>', '').replace('</li>', '')
-                    a += i + ', '
-    except FileNotFoundError:
-        print('Помилка функції write_to_file')
-        return None
-
-
-def get_supported_version(path):
-    is_version = False
-    a = str()
-    try:
-        with open(path, 'r', encoding='utf-8') as file:
-            for i in file.readlines():
-                i = i.strip()
-                if i.startswith('</supportedVersions>'):
-                    return a.removesuffix(' ')
-                if i.startswith('<supportedVersions>'):
-                    is_version = True
-                    continue
-                elif is_version is True:
-                    i = i.replace('<li>', '').replace('</li>', '')
-                    a += i + ' '
-    except FileNotFoundError:
-        print('Помилка функції write_to_file')
-        return None
-
-# Функція ініціалізації об'єкта класу Mod з всіма атрибутами. Повертає кортеж об'єктів.
-def create_mod_list():
+# Функція ініціалізації об'єкта класу Mod. Повертає список об'єктів.
+def create_mod_object_list():
     path_to_mod_folder = find_mod_folder()
+    mods = list()
 
-    subdirs = list()
     for i in path_to_mod_folder.iterdir():
         if i.is_dir():
-            subdirs.append(i.name)
-
-    mods = list()
-    for i in range(len(subdirs)):
-        path = path_to_mod_folder/subdirs[i]
-        path_to_about = path/'About'/'About.xml'
-        mods.append(Mod(packageid=subdirs[i],
-                        path=path,
-                        name=get_mod_name(path=path_to_about),
-                        author=get_mod_author(path=path_to_about),
-                        supported_versions=get_supported_version(path=path_to_about)
-                        )
-                    )
+            mods.append(Mod(path=i))
     return mods
-
-
-mod_list = create_mod_list()
-print()
-for i in range(len(mod_list)):
-    print('Об\'єкт', i, mod_list[i].return_atribute())
--- UI.py
+++ UI.py
@@ -1,59 +1,40 @@
 import customtkinter
-import Main
-
-
-class ModList(customtkinter.CTkScrollableFrame):
-    def __init__(self, master):
-        super().__init__(master)
-        self.name = Main.get_mod_names()
-        print(self.name)
-        self.mod_list = []
-        # self.path_to_mod = Main.
-
-        for i, name in enumerate(self.name):
-            mod_name = customtkinter.CTkLabel(self, text=name)
-            mod_name.configure(font=("Arial", 20))
-            mod_name.bind("<Double-Button-1>", self.clickable)
-            mod_name.grid(row=i, column=0, padx=10, pady=(0, 5), sticky="w")
-            self.mod_list.append(mod_name)
-
-    def clickable(self, event):
-        print("Натиснуто")
-
-    def copy(self, event):
-        self.clipboard_clear()
-        self.clipboard_append(event.widget.cget("text").replace("id-", ""))
+import ModificationClass
 
 
 class App(customtkinter.CTk):
+    mod_object_list = ModificationClass.create_mod_object_list()
+
     def __init__(self):
         # Налаштування вікна
         super().__init__()
+        S
         self.title("Конструктор перекладу")
         self.geometry("800x600")
-        self.minsize(420, 600)
-        self.maxsize(420, 600)
+        #self.minsize(420, 600)
+        #self.maxsize(420, 600)
         self.configure()
-        #self.config(bg="#1f1f1f")
 
         # Сітка
-        self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=0)
+        self.grid_rowconfigure(1, weight=1)
 
-        # Вікно списку модів
-        self.ModListFrame = customtkinter.CTkFrame(self)
-        self.ModListFrame.configure(fg_color="#1e1f22", corner_radius=0)
-        self.ModListFrame.grid(row=0, column=1, padx=0, pady=0, sticky="ens")
-        # Рядок пошуку в списку моодів
-        self.SearchString = customtkinter.CTkEntry(self.ModListFrame)
-        self.SearchString.configure(width=400, height=35, corner_radius=0, fg_color="#2b2d30", border_color="", font=("Arial", 15), placeholder_text="Пошук (назва, автор, айді)")
-        self.SearchString.grid(row=0, column=0, padx=10, pady=10, sticky="ens")
+        # Рамка списку модів
+        self.ModListFrame = customtkinter.CTkScrollableFrame(self)
+        self.ModListFrame.configure(corner_radius=0)
+        self.ModListFrame.grid(row=1, column=0, padx=0, pady=0, sticky="wens")
 
-        self.mod_list = ModList(self.ModListFrame)
-        self.mod_list.configure(height=500, fg_color="#1e1f22", corner_radius=0)
-        self.mod_list.grid(row=1, column=0, padx=10, pady=10, sticky="we")
+        # Рядок пошуку
+        self.SearchString = customtkinter.CTkEntry(self)
+        self.SearchString.configure(width=400, height=35, corner_radius=0, font=("Arial", 15), placeholder_text="Пошук")
+        self.SearchString.grid(row=0, column=0, padx=10, pady=10, sticky="wn")
+
+        for i in range(len(self.mod_object_list)):
+            self.ModLabel = customtkinter.CTkLabel(self.ModListFrame)
+            self.ModLabel.configure(text=self.mod_object_list[i].return_name())
+            self.ModLabel.grid(row=i+1, column=0, padx=10, pady=0, sticky="w")
+            print(self.mod_object_list[i].return_name())
 
 
 app = App()
 app.mainloop()
-

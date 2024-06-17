13df8c7baefd9255d2c85433db0f182a1ef75013
oleksandr2749 <oleksandr2749@gmail.com>
2024-06-17 20:14:22 +0300

    Вікно отримує список модів, що формується з об'єкта мода. Подвійне настискання на мод друкує про це(база для виклику функції)

--- UI.py
+++ UI.py
@@ -3,12 +3,10 @@ import ModificationClass
 
 
 class App(customtkinter.CTk):
-    mod_object_list = ModificationClass.create_mod_object_list()
-
-    def __init__(self):
+    def __init__(self, mod_object_list):
         # Налаштування вікна
         super().__init__()
-        S
+
         self.title("Конструктор перекладу")
         self.geometry("800x600")
         #self.minsize(420, 600)
@@ -29,12 +27,17 @@ class App(customtkinter.CTk):
         self.SearchString.configure(width=400, height=35, corner_radius=0, font=("Arial", 15), placeholder_text="Пошук")
         self.SearchString.grid(row=0, column=0, padx=10, pady=10, sticky="wn")
 
-        for i in range(len(self.mod_object_list)):
+        for i, mod_object in enumerate(mod_object_list):
             self.ModLabel = customtkinter.CTkLabel(self.ModListFrame)
-            self.ModLabel.configure(text=self.mod_object_list[i].return_name())
+            self.ModLabel.configure(text=mod_object.return_name())
+            self.ModLabel.bind("<Double-Button-1>", lambda event, idx=i: self.clickable(event, idx))
             self.ModLabel.grid(row=i+1, column=0, padx=10, pady=0, sticky="w")
-            print(self.mod_object_list[i].return_name())
+
+    def clickable(self, event, idx):
+        print(f'Натиснуто {mod_object_list[idx].return_name()}')
 
 
-app = App()
+mod_object_list = ModificationClass.create_mod_object_list()
+
+app = App(mod_object_list)
 app.mainloop()

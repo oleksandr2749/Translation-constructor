3563103eda282f2b2c216ee91148a7b6b6e08a8b
oleksanlr <oleksandr2749@gmail.com>
2024-07-09 11:34:26 +0300

    Нерозширений робочий

--- Process.py
+++ Process.py
@@ -1,7 +1,19 @@
 from pathlib import Path
+import shutil
+
+
+# функція повернення даних з файлу у вигляді списку рядків
+def get_data_from_file(path):
+    try:
+        with open(path.absolute(), 'r', encoding='utf-8') as file:
+            return file.readlines()
+    except FileNotFoundError:
+        print(f'Помилка виконання функції get_data_from_file! Не коректний шлях {path}')
+        return None
 
 
 # Функція створення списку відфільтрованих даних отриманих від get_data_from_file
+# Потребує вдосконалення: спрощення
 def get_filtered_data(obtained_data):
     data_strip = list()
     data = list()
@@ -9,7 +21,8 @@ def get_filtered_data(obtained_data):
     # Цикл видалення пробілів, табуляцій, переходів
     for i in obtained_data:
         data_strip.append(i.strip())
-    #
+    # Цикл фільтрування та формування готових рядків назв, описів
+    # та коментарів над ними, що містять оригінальний текст рядка
     for i in data_strip:
         if '<defName>' in i or '<label>' in i or '<description>' in i:
             if '<label>point</label>' not in i and '<label>edge</label>' not in i:
@@ -30,72 +43,83 @@ def get_filtered_data(obtained_data):
     return data
 
 
+# Функція створення та запису даних у файл
 def write_to_file(path: str, data: list):
     try:
         with open(path, 'w+', encoding='utf-8') as file:
+            # Запис першого рядка
             file.write('<?xml version=\'1.0\' encoding=\'utf-8\'?>\n<LanguageData>')
+            # Цикл запису рядків зі списку
             for i in data:
                 file.write(i)
+            # Запис останнього рядка
             file.write('\n</LanguageData>\n')
     except FileNotFoundError:
-        print('Помилка функції write_to_file')
+        print(f'Помилка виконання функції write_to_file! Не коректний шлях {path}')
         return None
 
 
-def run(path_to_mod=None):
-    path_to_mod = Path(path_to_mod)
-    path_for_folder_creation = Path('C:/Users/Олександр/Desktop/Тека файлів перекладу')
-    # path_for_folder_creation.mkdir()
-    # file_from_dir = get_file_from_dir(path_to_mod)
-    # print(test2[1])
+# Функція виконання
+def run(test1=None):
+    path_to_defs_folder = Path(test1)
 
-    for x in path_to_mod.iterdir():
-        for a in x.iterdir():
-            type = type_checking(a)
-            path_for_create_new_folder = path_for_folder_creation / a.parent.name
-            if type is True:
-                test1 = path_for_folder_creation / 'ThingDef'
-                if test1.exists() is False:
-                    test1.mkdir(parents=True)
-                write_to_file(path=str(test1 / a.name),
-                              data=get_filtered_data(get_data_from_file(a)))
-            else:
-                if path_for_create_new_folder.exists() is False:
-                    path_for_create_new_folder.mkdir(parents=False)
-                write_to_file(path=str(path_for_create_new_folder / a.name),
-                              data=get_filtered_data(get_data_from_file(a)))
+    path_to_languages_f = Path('/home/oleksanlr/Стільниця/Програма/Languages')
+    path_to_ukrainian_f = Path('/home/oleksanlr/Стільниця/Програма/Languages/Ukrainian')
+    path_to_definjected_f = Path('/home/oleksanlr/Стільниця/Програма/Languages/Ukrainian/DefInjected')
+    path_to_keyed_f = Path('/home/oleksanlr/Стільниця/Програма/Languages/Ukrainian/Keyed')
+    path_to_languages_f.mkdir()
+    path_to_ukrainian_f.mkdir()
+    path_to_definjected_f.mkdir()
+    path_to_keyed_f.mkdir()
+    paths = {'languages': path_to_languages_f, 'ukrainian': path_to_ukrainian_f, 'definjected': path_to_definjected_f,
+             'keyed': path_to_keyed_f}
 
-
-def type_checking(file):
-    is_defname = True
-    data = get_data_from_file(file)
-    for i in data:
-        if 'ThingDef' in i:
-            return is_defname
+    # отримання всіх файлів моду
+    def_file_list = process(path_to_defs_folder)
+    # створення теки, створення файлу, запис у файл, визначення типу файлу
+    for i in def_file_list:
+        if not paths['definjected'].joinpath(test2(i)).exists():
+            paths['definjected'].joinpath(test2(i)).mkdir()
+        write_to_file(str(paths['definjected'].joinpath(test2(i)).joinpath(i.name)), data=get_filtered_data(get_data_from_file(i)))
 
 
-def get_file_from_dir(path):
+def path_to_folder():
     pass
 
 
-def get_all_in_dir(path):
+def test2(file):
+    data = get_data_from_file(file)
+    file_type = str()
+    for i in data:
+        if 'ThingDef' in i:
+            file_type = 'ThingDef'
+            break
+    return file_type
+
+
+def process(path_to_defs):
     test = list()
-    for x in path.iterdir():
-        if x.is_file():
-            test.append(x)
-        elif x.is_dir():
-            test.append(x)
-            test.extend(get_all_in_dir(x))
+    for i in list(path_to_defs.iterdir()):
+        if i.is_file():
+            if 'ThingDef' == test2(i):
+                test.append(i)
+        elif i.is_dir():
+            test.extend(process(i))
     return test
 
 
-def get_data_from_file(path):
-    try:
-        with open(path.absolute(), 'r', encoding='utf-8') as file:
-            return file.readlines()
-    except FileNotFoundError:
-        print(path)
-        return None
+def delete_directory(directory_path):
+    path = Path(directory_path)
+
+    if path.exists():
+        shutil.rmtree(directory_path)
+        print(f"Тека '{directory_path}' успішно видалена\n")
+    else:
+        print(f"Тека '{directory_path}' не існує\n")
 
 
-run(path_to_mod='C:/Program Files (x86)/Steam/steamapps/workshop/content/294100/1718190143/1.5/Defs')
+# Очищення теки
+delete_directory('/home/oleksanlr/Стільниця/Програма')
+Path('/home/oleksanlr/Стільниця/Програма').mkdir()
+
+run(test1='/home/oleksanlr/.local/share/Steam/steamapps/workshop/content/294100/1888705256/1.5/Defs')

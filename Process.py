2fb7c7a945040b69d41879f63f466c5ae6a0ad2b
oleksanlr <oleksandr2749@gmail.com>
2024-07-14 14:14:07 +0300

    Допрацьовано

--- Process.py
+++ Process.py
@@ -1,17 +1,16 @@
 from pathlib import Path
+import shutil
 import xml.etree.ElementTree as ET
 
 
-def get_data():
-    tree = ET.parse('/home/oleksanlr/.steam/steam/steamapps/workshop/content/294100/3202046258/1.5/Defs/ThingDefs_Buildings/Buildings_Temperature.xml')
+def get_data(file):
+    tree = ET.parse(file)
     root = tree.getroot()
-    return get_data_process(root)
-
-
-def get_data_process(element):
     data = list()
-    for testDef in element:
-        data.append(testDef)
+
+    for tag in root:
+        if tag.find('label') is not None or tag.find('description') is not None:
+            data.append(tag)
     return data
 
 
@@ -21,7 +20,6 @@ def data_process(data):
         for defName in i.iter('defName'):
             pass
         for a in i.iter('label'):
-            ET.Comment(a.text)
             ET.SubElement(root, defName.text + '.label').text = a.text
         for a in i.iter('description'):
             ET.SubElement(root, defName.text + '.description').text = a.text
@@ -29,15 +27,29 @@ def data_process(data):
     return root
 
 
-print(get_data())
-root = data_process(get_data())
-print(ET.tostring(root, encoding='utf8').decode('utf8'))
-tree = ET.ElementTree(root)
-tree.write('test.xml', encoding='utf-8', xml_declaration=True)
+def process(file):
+    mod_data = get_data(file)
+
+    root = data_process(mod_data)
+    tree = ET.ElementTree(root)
+    save_path = Path('/home/oleksanlr/Стільниця/Програма/Languages/Ukrainian/DefInjected').joinpath(mod_data[0].tag)
+    if not save_path.exists():
+        save_path.mkdir()
+    tree.write(file_or_filename=save_path.joinpath(file.name), encoding='utf-8', xml_declaration=True)
+
+
+def get_all_defxml_path(path_to_defdir):
+    defxml_path_list = list()
+    for element in path_to_defdir.iterdir():
+        if element.is_file():
+            defxml_path_list.append(element)
+        else:
+            defxml_path_list.extend(get_all_defxml_path(element))
+    return defxml_path_list
 
 
 # Функція виконання
-def run():
+def run(modification):
     # словник шляхів тек моду перекладу
     mod_folder_paths = {'languages': Path('/home/oleksanlr/Стільниця/Програма/Languages'),
                 'ukrainian': Path('/home/oleksanlr/Стільниця/Програма/Languages/Ukrainian'),
@@ -48,16 +60,25 @@ def run():
     for i in mod_folder_paths.values():
         i.mkdir()
 
-    '''
-    # отримання всіх файлів моду
-    def_file_list = process(mod_folder)
-    # створення теки, створення файлу, запис у файл, визначення типу файлу
-    for i in def_file_list:
-        if not paths['definjected'].joinpath(file_type_check(i)).exists():
-            paths['definjected'].joinpath(file_type_check(i)).mkdir()
-        write_to_file(str(paths['definjected'].joinpath(file_type_check(i)).joinpath(i.name)),
-                      data=get_filtered_data(get_data_from_file(i)))
-    '''
+    defxml_path_list = get_all_defxml_path(path_to_defdir=Path(modification).joinpath('1.5/Defs'))
+
+    for defxml in defxml_path_list:
+        # print(defxml)
+        process(file=defxml)
 
 
-run()
+def delete_directory(directory_path):
+    path = Path(directory_path)
+
+    if path.exists():
+        shutil.rmtree(directory_path)
+        print(f"Тека '{directory_path}' успішно видалена\n")
+    else:
+        print(f"Тека '{directory_path}' не існує\n")
+
+
+# Очищення теки
+delete_directory('/home/oleksanlr/Стільниця/Програма')
+Path('/home/oleksanlr/Стільниця/Програма').mkdir()
+
+run(modification='/home/oleksanlr/.steam/steam/steamapps/workshop/content/294100/3202046258')

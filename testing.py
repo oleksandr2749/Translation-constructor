c0c6122a4f7119b3fbfa553fd0a96fd36f105992
oleksanlr <oleksandr2749@gmail.com>
2024-07-15 14:43:34 +0300

    тестування

--- testing.py
+++ testing.py
@@ -2,18 +2,14 @@ from pathlib import Path
 
 
 def search_294100_folder():
-    # Створюємо список кореневих каталогів для пошуку
-    root_paths = []
-    for p in Path('/').glob('*'):
-        if p.is_dir():
-            root_paths.append(Path(p))
-            print(p)
-
-    for root in root_paths:
-        for path in root.rglob('Steam/steamapps/workshop/content/294100'):
-            return path.resolve()
-
-    return None  # Повертаємо None, якщо шлях не знайдено
+    drives = ['C:', 'D:', 'E:', 'F:', 'G:', 'H:']
+    for drive in drives:
+        try:
+            for path in Path(drive).rglob('Steam/steamapps/workshop/content/294100'):
+                return path.resolve()
+        except PermissionError:
+            # Пропустити диски, до яких немає доступу
+            continue
 
 
 print(search_294100_folder())

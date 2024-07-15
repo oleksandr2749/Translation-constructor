0f7b2855e5a4ce35e2311c7acdfa6a6fac8a5f59
oleksanlr <oleksandr2749@gmail.com>
2024-07-15 14:38:38 +0300

    тестування

--- testing.py
+++ testing.py
@@ -2,8 +2,18 @@ from pathlib import Path
 
 
 def search_294100_folder():
-    for path in Path('/').rglob('Steam/steamapps/workshop/content/294100'):
-        return path.resolve()
+    # Створюємо список кореневих каталогів для пошуку
+    root_paths = []
+    for p in Path('/').glob('*'):
+        if p.is_dir():
+            root_paths.append(Path(p))
+            print(p)
+
+    for root in root_paths:
+        for path in root.rglob('Steam/steamapps/workshop/content/294100'):
+            return path.resolve()
+
+    return None  # Повертаємо None, якщо шлях не знайдено
 
 
 print(search_294100_folder())

a5060410e70cbfb4d54e7900a1441e5bf701703f
oleksanlr <oleksandr2749@gmail.com>
2024-07-15 14:26:57 +0300

    тестування

--- testing.py
+++ testing.py
@@ -1,25 +1,9 @@
-import os
 from pathlib import Path
 
 
-def get_all_drives():
-    drives = []
-    bitmask = os.sysconf("SC_DRIVES")
-    for i in range(26):
-        if bitmask & (1 << i):
-            drives.append(chr(65 + i) + ':\\')
-    return drives
-
-
 def search_294100_folder():
-    for drive in get_all_drives():
-        for path in Path(drive).rglob('Steam/steamapps/workshop/content/294100'):
-            return path.resolve()
+    for path in Path('/').rglob('Steam/steamapps/workshop/content/294100'):
+        return path.resolve()
 
 
-# Виклик функції для пошуку
-result = search_294100_folder()
-if result:
-    print(f"Folder found at: {result}")
-else:
-    print("Folder not found.")
+print(search_294100_folder())

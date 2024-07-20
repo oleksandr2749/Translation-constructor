0175e6e78e25bf89191761f78e6746a173aded6e
oleksandr2749 <143964638+oleksandr2749@users.noreply.github.com>
2024-07-20 17:01:28 +0300

    Delete testing.py

--- testing.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-def search_294100_folder():
-    volumes = ['/', 'A:', 'B:', 'C:', 'D:', 'E:', 'F:', 'G:', 'H:', 'I:', 'J:', 'K:', 'L:', 'M:', 'N:', 'O:', 'P:', 'Q:', 'R:', 'S:', 'T:', 'U:', 'V:', 'W:', 'X:', 'Y:', 'Z:',]
-
-    for volume in volumes:
-        for path in Path(volume).rglob('Steam/steamapps/workshop/content/294100'):
-            return path.resolve()
-
-print(search_294100_folder())
